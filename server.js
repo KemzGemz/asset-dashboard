c894c89144759c01db72930dfc45fc8ab4bf882c
Lukman-FGL <lukman.mohamad@forgelion.com>
2025-12-26 06:33:52 +0800

    update port in server.js

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use("/api", portfolioRoutes);
 // Serve frontend
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
